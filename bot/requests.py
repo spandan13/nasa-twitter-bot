1469f9a8cda5fa781c53f57cd9ffc51d6d432b93
spandan13 <spandanathaide@gmail.com>
2023-07-19 08:05:52 +0000

    Implemented Twitter Scrapper to search for tweets

--- bot/requests.py
+++ bot/requests.py
@@ -75,12 +75,33 @@ def get_img_file(media_link, id, temp, file_ext="jpg", apod=False, news=False):
                     image.write(chunk)
     return filename
 
-def mentions(bot_account, api):
+def mentions(bot_account, scrape):
     """Pulls recent mentions to the bot
         using the twitter api"""
     mentions = []
     search_text = (bot_account + ' ' + '-filter:retweets' + ' ' + '-from:' + bot_account)
-    for tweet in api.search_tweets(q=search_text, result_type='recent', count=5, include_entities='false'):
+    raw_data = scrape.run(
+        limit=10,
+        retries=1,
+        queries=[
+            {
+                'category': 'Latest',
+                'query': search_text
+            }
+        ]
+    )
+    for raw_tweet in raw_data[0]:
+        tweet_details = raw_tweet['content']['itemContent']['tweet_results']['result']
+        tweet_id = tweet_details['rest_id']
+        user_id = tweet_details['core']['user_results']['result']['rest_id']
+        user_name = tweet_details['core']['user_results']['result']['legacy']['screen_name']
+        created_at = tweet_details['legacy']['created_at']
+        full_text = tweet_details['legacy']['full_text']
+        try:
+            in_reply_to = tweet_details['legacy']['in_reply_to_status_id_str']
+        except KeyError:
+            in_reply_to = None
+        tweet = {'tweet_id':tweet_id,'user_id':user_id,'user_name':user_name,'created_at':created_at,'full_text':full_text,'in_reply_to':in_reply_to}
         mentions.append(tweet)
     return mentions
 
@@ -97,7 +118,8 @@ def is_recent(tweet, time_in_minutes):
     """Return true if tweet is recent when compared
     to the time in minutes as per settings"""
     expiration_time = datetime.timedelta(minutes=time_in_minutes)
-    tweet_date = (tweet.created_at).replace(tzinfo=None)
+    created_at = datetime.datetime.strptime(tweet['created_at'], "%a %b %d %H:%M:%S %z %Y")
+    tweet_date = (created_at).replace(tzinfo=None)
     time_since_order = datetime.datetime.now() - tweet_date
     return time_since_order < expiration_time
 
@@ -105,14 +127,14 @@ def already_answered(tweet, log):
     """Return true if tweet id found in log"""
     with open(log, 'r') as log:
         for line in log:
-            if str(tweet.id) in line:
+            if str(tweet['tweet_id']) in line:
                 return True
         else:
             return False
 
 def is_img_request(tweet, bot_account, command):
     """Return True if mention start with the request_command."""
-    str_mention = (" " + tweet.text.lower() + " ")
+    str_mention = (" " + tweet['full_text'].lower() + " ")
     if re.search("\s" + command.lower() + "\S*\s+" + bot_account.lower() + "\s+.*?", str_mention):
         return True
     else:
@@ -122,7 +144,7 @@ def master_mentions(mentions, log, master):
     "All the mentions to the bot from the master account"
     master_mentions = []
     for tweet in mentions:
-        if ('@' + tweet.user.screen_name).lower() == master and not already_answered(tweet, log):
+        if ('@' + tweet['user_name']).lower() == master and not already_answered(tweet, log):
             master_mentions.append(tweet)
     return master_mentions
 
@@ -136,7 +158,7 @@ def blocked(user_name, blocked_list):
 
 def is_delete_order(tweet, ban_command):
     """Returns true if request is a delete command"""
-    mention = tweet.text.lower()
+    mention = tweet['full_text'].lower()
     ban_command = ban_command.lower()
     return ban_command in mention
 
