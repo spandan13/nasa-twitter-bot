4ec1aed11a58cef9a707d665eefe278b4a3d06cd
spandan13 <spandanathaide@gmail.com>
2023-07-19 07:09:19 +0000

    Added config for Twitter API v2 & for Tweet Scrapper

--- settings/config.py
+++ settings/config.py
@@ -2,6 +2,7 @@ import os
 import configparser
 import tweepy
 import ast
+from twitter.search import Search
 
 """Reads the settings file which holds all values and keys"""
 
@@ -16,6 +17,8 @@ twt_secret_key = api_config['twt_secret_key']
 twt_token = api_config['twt_token']
 twt_secret_token = api_config['twt_secret_token']
 nasa_api_key = api_config['nasa_api_key']
+ct0 = api_config['ct0']
+auth_token = api_config['auth_token']
 
 app_config = config['App']
 allow_repeat_after = int(app_config['allow_repeat_after'])
@@ -37,6 +40,12 @@ text_config = config['Texts']
 tweet_text = text_config['tweet_text']
 
 #Twitter Api
-auth = tweepy.OAuthHandler(twt_api_key, twt_secret_key)
-auth.set_access_token(twt_token, twt_secret_token)
-api = tweepy.API(auth)
+auth_v1 = tweepy.OAuthHandler(twt_api_key, twt_secret_key)
+auth_v1.set_access_token(twt_token, twt_secret_token)
+api = tweepy.Client(
+    consumer_key=twt_api_key,
+    consumer_secret=twt_secret_key,
+    access_token=twt_token,
+    access_token_secret=twt_secret_token
+)
+scrape = Search(cookies={"ct0": ct0, "auth_token": auth_token})
